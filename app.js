76f851f583f53f81a2066883f74238c58e66f649
Ajith Prakash <ajiprak@gmail.com>
2015-09-16 07:24:38 +0530

    Async code

--- app.js
+++ app.js
@@ -6,3 +6,26 @@ exports.sanitize = function(word){
 exports.tokenize = function(sentence){
     return sentence.split(' ');
 };
+
+exports.info = function(callback){
+    var https = require('https');
+    var options = {
+        url:'api.github.com',
+        path:'/repos/sayanee/build-podcast',
+        method:'GET',
+        headers : {'User-Agent':'saynee'}
+               
+    };
+    var str = '';
+    https.request(options,function(response){
+        response.on('data',function(data){
+            str += data; 
+        });
+        response.on('end',function(){
+            callback(JSON.parse(str));
+        });
+        response.on('error',function(error){
+            callback(errror);
+        });
+    });
+};
